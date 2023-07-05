20ee0433d66f9aea6cfab6ee9cb928e67f3c1513
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-07-05 16:02:37 +0200

    EDIT esercizio base completato

--- js/my-script.js
+++ js/my-script.js
@@ -3,11 +3,30 @@ const {createApp}=Vue;
 createApp({
     data(){
         return{
+            emails:[],
 
         }
     },
     method:{
         
+    
+    },
+    created(){
+
+        for (let index = 0; index < 10; index++) {
+                
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+           .then((response)=>{
+               const result=response.data.response;
+               this.emails.push(result);
+               console.log("result",result);
+               console.log("emails",this.emails)
+           })
+            
+        }
+        
+
+
     }
 
 }).mount("#app")
